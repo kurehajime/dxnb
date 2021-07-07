dd921b8621cd0560c6f692713617628712bd60fd
kurehajime <xiidec@gmail.com>
2021-07-07 22:08:01 +0900

    change speed

--- src/params.ts
+++ src/params.ts
@@ -24,7 +24,7 @@ export enum State {
 
 export const MAX_ROW_COUNT = 11
 export const MAX_COLUMN_COUNT = 5
-export const PROGRESS_SPAN = 300 * 5
+export const PROGRESS_SPAN = 200 * 5
 export const STEP_SPAN = 50
 
 export const FRAME_TOP = 10
